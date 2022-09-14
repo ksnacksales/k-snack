3a32cada1c69af4c1a361c024f1c2d0327f33ba5
USER <ksnacksales@gmail.com>
2022-09-14 22:42:13 +0900

    2022.09.14 Git Project 생성

---
+++ src/main/java/com/ksnack/KSnackApplication.java
@@ -0,0 +1,15 @@
+package com.ksnack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KSnackApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(KSnackApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/ksnack/KSnackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksnack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KSnackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
